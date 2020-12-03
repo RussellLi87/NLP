f614c0dfd78c64ee6e348812d84fd99bd765533b
rcli <russellli87@hotmail.com>
2020-12-03 15:09:01 +0800

    complete a w2v model

---
+++ word2vec.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import os
+import re
+import shutil
+import string
+import tensorflow as tf
+
+# from keras import layers
+# from keras import losses
+# from keras import preprocessing
+# from keras.layers.experimental.preprocessing import TextVectorization
+
+
+print(tf.__version__)
